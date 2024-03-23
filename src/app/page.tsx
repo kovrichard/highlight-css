e8a53f066fd42a903702c522d6af944de3d32140
Richard Kovacs <richardkovacs.it@gmail.com>
2024-03-23 13:10:22 +0100

    feat: add default use case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,187 @@
-import Image from "next/image";
+"use client";
+
+import { useRef, useState } from "react";
+
+enum Style {
+  Gradient,
+}
 
 export default function Home() {
+  const [text, setText] = useState("Hello World!");
+  const textAreaRef = useRef<HTMLPreElement>(null);
+  const [style, setStyle] = useState<Style>(Style.Gradient);
+  const [margin, setMargin] = useState<{ [key: string]: number }>({
+    top: 0,
+    right: -0.4,
+    bottom: 0,
+    left: -0.4,
+  });
+  const [padding, setPadding] = useState<{ [key: string]: number }>({
+    top: 0.1,
+    right: 0.4,
+    bottom: 0.1,
+    left: 0.4,
+  });
+  const [borderRadius, setBorderRadius] = useState<{ [key: string]: number }>({
+    topLeft: 0.8,
+    topRight: 0.3,
+    bottomLeft: 0.3,
+    bottomRight: 0.8,
+  });
+
+  const copyToClipboard = async () => {
+    const text = textAreaRef.current?.innerText;
+    await navigator.clipboard.writeText(text!);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+      <h1 className="text-4xl">Mark Highlighter</h1>
+      <h2 className="text-2xl">Generate highlighter effect CSS code</h2>
+      <div className="flex flex-col sm:flex-row items-center justify-around w-full">
+        <div className="flex flex-col items-center gap-8">
+          <mark
+            style={{
+              marginTop: `${margin.top}em`,
+              marginRight: `${margin.right}em`,
+              marginBottom: `${margin.bottom}em`,
+              marginLeft: `${margin.left}em`,
+              paddingTop: `${padding.top}em`,
+              paddingRight: `${padding.right}em`,
+              paddingBottom: `${padding.bottom}em`,
+              paddingLeft: `${padding.left}em`,
+              borderTopLeftRadius: `${borderRadius.topLeft}em`,
+              borderTopRightRadius: `${borderRadius.topRight}em`,
+              borderBottomLeftRadius: `${borderRadius.bottomLeft}em`,
+              borderBottomRightRadius: `${borderRadius.bottomRight}em`,
+              background: "transparent",
+              backgroundImage:
+                "linear-gradient(to right, rgba(255, 225, 0, 0.1), rgba(255, 225, 0, 0.7) 4%, rgba(255, 225, 0, 0.3))",
+              WebkitBoxDecorationBreak: "clone",
+              boxDecorationBreak: "clone",
+            }}
+            className="text-4xl"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {text}
+          </mark>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="w-64 p-2"
+            style={{ color: "black" }}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col items-center p-8 bg-slate-500 rounded-3xl">
+          <h2 className="text-2xl">Settings</h2>
+          <label className="text-2xl">Style</label>
+          <select
+            value={style}
+            onChange={(e) => setStyle(parseInt(e.target.value))}
+            className="w-64 p-2 text-slate-900"
+          >
+            <option value={Style.Gradient}>Gradient</option>
+          </select>
+          <label className="text-2xl">Margin</label>
+          <div className="flex justify-around gap-4">
+            {["top", "right", "bottom", "left"].map((direction) => (
+              <input
+                key={direction}
+                type="number"
+                value={margin[direction]}
+                onChange={(e) =>
+                  setMargin({
+                    ...margin,
+                    [direction]: parseFloat(e.target.value),
+                  })
+                }
+                className="w-16 p-2 text-slate-900"
+              />
+            ))}
+          </div>
+          <label className="text-2xl">Padding</label>
+          <div className="flex justify-around gap-4">
+            {["top", "right", "bottom", "left"].map((direction) => (
+              <input
+                key={direction}
+                type="number"
+                value={padding[direction]}
+                onChange={(e) =>
+                  setPadding({
+                    ...padding,
+                    [direction]: parseFloat(e.target.value),
+                  })
+                }
+                className="w-16 p-2 text-slate-900"
+              />
+            ))}
+          </div>
+          <label className="text-2xl">Border Radius</label>
+          <div className="flex justify-around gap-4">
+            {["topLeft", "topRight", "bottomLeft", "bottomRight"].map(
+              (direction) => (
+                <input
+                  key={direction}
+                  type="number"
+                  value={borderRadius[direction]}
+                  onChange={(e) =>
+                    setBorderRadius({
+                      ...borderRadius,
+                      [direction]: parseFloat(e.target.value),
+                    })
+                  }
+                  className="w-16 p-2 text-slate-900"
+                />
+              )
+            )}
+          </div>
+          <div className="relative">
+            <button
+              className="absolute top-2 right-2 bg-green-700 rounded py-1 px-2"
+              onClick={copyToClipboard}
+            >
+              Copy
+            </button>
+            <pre ref={textAreaRef} className="bg-black p-8 rounded-lg">
+              <code className="flex flex-col">
+                <div>
+                  <span className="text-[#66d9ef]">margin:</span>{" "}
+                  <span>
+                    {margin.top}em {margin.right}em {margin.bottom}em{" "}
+                    {margin.left}em;
+                  </span>
+                </div>
+                <div>
+                  <span className="text-[#66d9ef]">padding:</span>{" "}
+                  <span>
+                    {padding.top}em {padding.right}em {padding.bottom}em{" "}
+                    {padding.left}em;
+                  </span>
+                </div>
+                <div>
+                  <span className="text-[#66d9ef]">border-radius:</span>{" "}
+                  <span>
+                    {borderRadius.topLeft}em {borderRadius.topRight}
+                    em {borderRadius.bottomLeft}em {borderRadius.bottomRight}em;
+                  </span>
+                </div>
+                <div>
+                  <span className="text-[#66d9ef]">background:</span>{" "}
+                  <span>transparent;</span>
+                </div>
+                <div>
+                  <span className="text-[#66d9ef]">background-image:</span>{" "}
+                  <span>linear-gradient(</span>
+                </div>
+                <span>{"  "}to right,</span>
+                <span>{"  "}rgba(255, 225, 0, 0.1),</span>
+                <span>{"  "}rgba(255, 225, 0, 0.7) 4%,</span>
+                <span>{"  "}rgba(255, 225, 0, 0.3)</span>
+                <span>);</span>
+              </code>
+            </pre>
+          </div>
+        </div>
       </div>
     </main>
   );
