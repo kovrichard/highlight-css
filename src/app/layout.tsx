58c8780033a1b5420c42f470e7c92eba5b05ca64
Richard Kovacs <richardkovacs.it@gmail.com>
2024-03-23 13:58:59 +0100

    feat: add link to twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`relative ${inter.className}`}>
+        <div className="absolute flex items-center top-4 right-4 gap-4">
+          <Image
+            src="/richard-kovacs.webp"
+            alt="Richard Kovacs"
+            className="w-12 h-12 rounded-full"
+            width={48}
+            height={48}
+          />
+          <a
+            href="https://twitter.com/rchardkovacs"
+            className="text-lg font-semibold text-[#1da1f2]"
+          >
+            Follow Me on X
+          </a>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
