81c5bc7bf6c5c07a7630f6aa7b762da20c9ce66e
Richard Kovacs <richardkovacs.it@gmail.com>
2024-03-29 17:09:22 +0100

    feat: add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "../components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -53,34 +54,41 @@ export default function RootLayout({
       <body
         className={`relative min-h-screen min-w-[375px] ${inter.className}`}
       >
-        <div className="absolute flex items-center top-4 right-4 gap-4">
-          <Image
-            src="/richard-kovacs.webp"
-            alt="Richard Kovacs"
-            className="w-12 h-12 rounded-full"
-            width={48}
-            height={48}
-          />
-          <a
-            href="https://twitter.com/rchardkovacs"
-            target="_blank"
-            className="text-lg font-semibold text-[#1da1f2]"
-          >
-            Follow Me on X
-          </a>
-        </div>
-        {children}
-        <Toaster />
-        <footer className="absolute bottom-0 left-0 right-0 p-4 text-center">
-          Built by{" "}
-          <a
-            href="https://richardkovacs.dev"
-            target="_blank"
-            className="underline"
-          >
-            Richard Kovacs
-          </a>
-        </footer>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="absolute flex items-center top-4 right-4 gap-4">
+            <Image
+              src="/richard-kovacs.webp"
+              alt="Richard Kovacs"
+              className="w-12 h-12 rounded-full"
+              width={48}
+              height={48}
+            />
+            <a
+              href="https://twitter.com/rchardkovacs"
+              target="_blank"
+              className="text-lg font-semibold text-[#1da1f2]"
+            >
+              Follow Me on X
+            </a>
+          </div>
+          {children}
+          <Toaster />
+          <footer className="absolute bottom-0 left-0 right-0 p-4 text-center">
+            Built by{" "}
+            <a
+              href="https://richardkovacs.dev"
+              target="_blank"
+              className="underline"
+            >
+              Richard Kovacs
+            </a>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
